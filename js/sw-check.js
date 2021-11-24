96f76ac3a8a1ed4e286c86caedf3187154b41a33
Matias Bubello <matt.bubello@gmail.com>
2021-11-24 19:11:32 -0300

    online-ofline status

--- js/sw-check.js
+++ js/sw-check.js
@@ -1,15 +1,31 @@
 if ('serviceWorker' in navigator){
     navigator.serviceWorker.register("../service-worker.js").then((message) => {
-        console.log("Service Worker esta listo!");
+        // console.log("Service Worker esta listo!");
     });
 } else {
     console.log('Service Worker no es soportado!');
 }
 
-window.addEventListener('offline', (e) => {
-    console.log('Estoy Offline!');
+window.addEventListener('offline', event => {
+    /*console.log('Estoy Offline!');*/
+    let status = document.querySelector('.status'),
+        statusText = document.querySelector('.status-text');
+    status.classList.replace('online','offline');
+    statusText.innerHTML = "Offline";
 })
 
-window.addEventListener('online', (e) =>{
-    console.log('Estoy online');
+window.addEventListener('online', event =>{
+    // console.log('Estoy online');
+    let status = document.querySelector('.status'),
+        statusText = document.querySelector('.status-text');
+    status.classList.replace('offline','online');
+    statusText.innerHTML = "Online";
 })
+
+if (!navigator.onLine){
+    // console.log('Sin conexión!');
+    let status = document.querySelector('.status'),
+        statusText = document.querySelector('.status-text');
+    status.classList.replace('online','offline');
+    statusText.innerHTML = "Offline";
+}
